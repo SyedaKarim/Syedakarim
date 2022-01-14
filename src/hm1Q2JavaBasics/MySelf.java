97abc91356b02068e5e76c359074f4ddc7d1a1b8
SyedaKarim <syedahakarim@gmail.com>
2022-01-13 22:06:34 -0500

    01-13-2022 10:06pm

---
+++ src/hm1Q2JavaBasics/MySelf.java
@@ -0,0 +1,13 @@
+package hm1Q2JavaBasics;
+
+public class MySelf {
+	
+	public static void main(String[] args) {
+	System.out.println ("Syeda Karim");	
+		
+		
+	}
+	
+	
+
+}
---
+++ src/hw2Q2JavaBasics/MyCar.java
@@ -0,0 +1,12 @@
+package hw2Q2JavaBasics;
+
+public class MyCar {
+
+	public static void main(String[] args) {
+	     System.out.print("My name is Syeda Karim.  I don't have Car");
+	     System.out.print(" But I will buy Car soon. + I will but Toyota C-HRs");
+	     System.out.print(" I heard Toyota C-HR's are subcompact Suvs. And the color will be Red because red is my mom's favorite color");
+	     
+	}
+
+}
